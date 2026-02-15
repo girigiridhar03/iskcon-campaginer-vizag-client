a23fec0686d0818b90aa978a0ac7e1921841cfe1
Giridhar D <girigiridhar0603@gmail.com>
2026-02-15 20:18:23 +0530

    vercel changes

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000, // 1000 KB
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
